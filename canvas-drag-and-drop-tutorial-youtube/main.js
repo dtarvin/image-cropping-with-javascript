6f3d18a87f5421eb1279a2d0cb360caa9fb9b341
dtarvin <david.tarvin@gmail.com>
2023-09-15 10:21:27 -0500

    merging programs

--- canvas-drag-and-drop-tutorial-youtube/main.js
+++ canvas-drag-and-drop-tutorial-youtube/main.js
@@ -1,19 +1,38 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
+let img = document.getElementById("mountain");
 
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 10;
+context.drawImage(img, 0, 0);
+
+canvas.width = 2048;
+canvas.height = 1536;
 
 canvas.style.border = '5px solid red';
 
 let canvas_width = canvas.width;
 let canvas_height = canvas.height;
+let offset_x;
+let offset_y;
+
+let get_offset = function () {
+    let canvas_offsets = canvas.getBoundingClientRect();
+    offset_x = canvas_offsets.left;
+    offset_y = canvas_offsets.top;
+}
+
+get_offset();
+window.onscroll = function () { get_offset();  }
+window.onresize = function () { get_offset();  }
+canvas.onresize = function () { get_offset();  }
 
 let shapes = [];
 let current_shape_index = null;
 let is_dragging = false;
-shapes.push({ x: 200, y: 50, width: 200, height: 200, color: 'red' });
-shapes.push({ x:10, y: 10, width: 100, height: 100, color: 'blue' });
+let startX;
+let startY;
+
+shapes.push({ x: 200, y: 50, width: 200, height: 200, color: 'transparent' });
+shapes.push({ x: 10, y: 10, width: 100, height: 100, color: 'blue' });
 
 let is_mouse_in_shape = function (x, y, shape) {
     let shape_left = shape.x;
@@ -31,8 +50,8 @@ let is_mouse_in_shape = function (x, y, shape) {
 let mouse_down = function (event) {
     event.preventDefault();
 
-    let startX = parseInt(event.clientX);
-    let startY = parseInt(event.clientY);
+    startX = parseInt(event.clientX - offset_x);
+    startY = parseInt(event.clientY - offset_y);
 
     let index = 0;
     for (let shape of shapes) {
@@ -63,6 +82,29 @@ let mouse_out = function (event) {
     is_dragging = false;
 }
 
+let mouse_move = function (event) {
+    if (!is_dragging) {
+        return;
+    } else {
+        event.preventDefault();
+        let mouseX = parseInt(event.clientX - offset_x);
+        let mouseY = parseInt(event.clientY - offset_y);
+
+        let dx = mouseX - startX;
+        let dy = mouseY - startY;
+
+        let current_shape = shapes[current_shape_index];
+
+        current_shape.x += dx;
+        current_shape.y += dy;
+
+        draw_shapes();
+
+        startX = mouseX;
+        startY = mouseY;
+    }
+}
+
 canvas.onmousedown = mouse_down;
 canvas.onmouseup = mouse_up;
 canvas.onmouseout = mouse_out;
@@ -70,9 +112,11 @@ canvas.onmousemove = mouse_move;
 
 let draw_shapes = function () {
     context.clearRect(0, 0, canvas_width, canvas_height);
+    context.drawImage(img, 0, 0);
     for (let shape of shapes) {
-        context.fillStyle = shape.color;
-        context.fillRect(shape.x, shape.y, shape.width, shape.height);
+        context.strokeStyle = 'green';
+        context.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        context.stroke();
     }
 }
 
